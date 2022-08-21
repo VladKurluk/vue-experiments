8d617fcd749e9f141016393435a0602e480735f0
Vlad Kurluk <vlad11kurluk89@gmail.com>
2022-08-21 18:28:47 +0300

    Added city view and redirect to then after click on geolocation data

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -21,6 +21,7 @@
             v-for="searchResult in mapboxSearchResults"
             :key="searchResult.id"
             class="py-2 cursor-pointer"
+            @click="previewCity(searchResult)"
           >
             {{ searchResult.place_name }}
           </li>
@@ -33,6 +34,7 @@
 <script setup>
 import { ref } from "vue";
 import axios from "axios";
+import { useRouter } from "vue-router";
 
 const searchQuery = ref("");
 const queryTimeout = ref(null);
@@ -56,5 +58,22 @@ const getSearchResults = () => {
 
     mapboxSearchResults.value = null;
   }, 1000);
+};
+
+const router = useRouter();
+const previewCity = (searchResult) => {
+  const [city, state] = searchResult.place_name.split(",");
+  router.push({
+    name: "cityView",
+    params: {
+      state: state.replaceAll(" ", ""),
+      city,
+    },
+    query: {
+      lat: searchResult.geometry.coordinates[1],
+      lng: searchResult.geometry.coordinates[0],
+      preview: true,
+    },
+  });
 };
 </script>
