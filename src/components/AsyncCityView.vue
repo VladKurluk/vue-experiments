9d53b01ed46a4f33a202091484febe1c3bf4e293
Vlad Kurluk <vlad11kurluk89@gmail.com>
2022-08-31 22:34:56 +0300

    Use Suspense component

---
+++ src/components/AsyncCityView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div></div>
+</template>
+
+<script setup>
+import axios from "axios";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+const getWeatherData = async () => {
+  try {
+    const weatherData = await axios.get(
+      //   `https://api.openweathermap.org/data/3.0/onecall?lat=${route.query.lat}&lon=${route.query.lng}&appid=${process.env.VUE_APP_OPEN_WEATHER_KEY}&units=imperial`
+      //   `https://api.openweathermap.org/data/2.5/weather?lat=${route.query.lat}&lon=${route.query.lng}&appid=${process.env.VUE_APP_OPEN_WEATHER_KEY}&lang=ru`
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${route.query.lat}&lon=${route.query.lng}&appid=${process.env.VUE_APP_OPEN_WEATHER_KEY}&units=imperial&lang=ru`
+    );
+
+    return weatherData.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+// eslint-disable-next-line no-unused-vars
+const weatherData = await getWeatherData();
+</script>
